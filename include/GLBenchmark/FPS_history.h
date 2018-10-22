a74937d6f0a7d41dcd7cfa7d152650f72627371e
Stan Slupecki <sslup@hermetikos.com>
2018-10-22 00:24:17 -0400

    Adds history and fps history trackers

---
+++ include/GLBenchmark/FPS_history.h
@@ -0,0 +1,26 @@
+/*
+This file is part of OpenGL-Cross-Platform-Benchmark which is released under the MIT License.
+Copyright Stan Slupecki 2018
+Go to https://misturdust319.github.io/OpenGL-Cross-Platform-Benchmark/ for full license details.
+*/
+
+#ifndef FPS_HIST_H
+#define FPS_HIST_H
+
+#include <GLBenchmark/history.h>
+
+#include <iostream>
+#include <string>
+
+class FPSHistory : public History {
+public: 
+	// constructor
+	FPSHistory();
+	// addItem overload that simplifies including
+	// FPS, timestamp
+	void addItem(std::string&, std::string&);
+};
+
+#endif //FPS_HIST_H
+
+
