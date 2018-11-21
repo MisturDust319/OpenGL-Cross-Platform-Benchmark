f44b35630d83a19605c21b777606ea397fc8c3b5
Stan Slupecki <sslup@hermetikos.com>
2018-11-21 00:21:04 -0500

    Now generates Cube from Cube class

--- src/cube.cpp
+++ src/cube.cpp
@@ -11,45 +11,34 @@ Cube::Cube(int ID, glm::vec3 origin)
 	: Shape(ID, origin) {
 
 	// init vertices
-	// float vertices[] = {
-	//     -0.5, -0.5,  0.5,
-	//      0.5, -0.5,  0.5,
-	//      0.5,  0.5,  0.5,
-	//     -0.5,  0.5,  0.5,
-	//     -0.5, -0.5, -0.5,
-	//      0.5, -0.5, -0.5,
-	//      0.5,  0.5, -0.5,
-	//     -0.5,  0.5, -0.5,
-	//   };
-
 	float vertices[] = {
-         0.5f,  0.5f, 0.0f,  // top right
-         0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
+	    -0.5, -0.5,  0.5,
+	     0.5, -0.5,  0.5,
+	     0.5,  0.5,  0.5,
+	    -0.5,  0.5,  0.5,
+	    -0.5, -0.5, -0.5,
+	     0.5, -0.5, -0.5,
+	     0.5,  0.5, -0.5,
+	    -0.5,  0.5, -0.5,
+	};
 
-	Shape::setVertices(vertices, 12);
+	Shape::setVertices(vertices, 24);
 	// init indices
- 	// 	int indices[] = {
-	// 	0, 1, 2,
-	// 	2, 3, 0,
-	// 	1, 5, 6,
-	// 	6, 2, 1,
-	// 	7, 6, 5,
-	// 	5, 4, 7,
-	// 	4, 0, 3,
-	// 	3, 7, 4,
-	// 	4, 5, 1,
-	// 	1, 0, 4,
-	// 	3, 2, 6,
-	// 	6, 7, 3,
-	// };
-	unsigned int indices[] = {  // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
-    };	
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0,
+		1, 5, 6,
+		6, 2, 1,
+		7, 6, 5,
+		5, 4, 7,
+		4, 0, 3,
+		3, 7, 4,
+		4, 5, 1,
+		1, 0, 4,
+		3, 2, 6,
+		6, 7, 3,
+	};
 
-	Shape::setIndices(indices, 6);
+	Shape::setIndices(indices, 36);
 
 }
