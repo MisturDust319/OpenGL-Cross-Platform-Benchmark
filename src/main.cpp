4e323f26f8b454590581d50303bd6e9e8d09eb36
Stan Slupecki <sslup@hermetikos.com>
2018-12-01 22:13:04 -0500

    Adds OpenGL info printout

--- src/main.cpp
+++ src/main.cpp
@@ -64,6 +64,11 @@ public:
 		glutInitWindowSize(SCR_WIDTH, SCR_HEIGHT);
 		glutCreateWindow("OpenGL Benchmark");
 
+		// print OpenGL info to terminal
+		std::cout << "GL Renderer: " << glGetString(GL_RENDERER) << std::endl
+			<< "GL Version: " << glGetString(GL_VERSION) << std::endl
+			<< "GL Vendor: " << glGetString(GL_VENDOR);
+
 		// init GLEW
 		glewInit();
 
